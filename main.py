6fd6797ef685166c91086a079150494a45a16251
jMoscato <110347584+moscato@users.noreply.github.com>
2023-06-25 06:56:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, render_template, request, jsonify
+
+from chat import get_response
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+@app.route("/")
+def index_get():
+    return render_template("base.html")
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+@app.post("/predict")
+def predict():
+    text = request.get_json().get("message")
+    # TODO: check is text is valid
+    response = get_response(text)
+    message = {"answer": response}
+    return jsonify(message)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
